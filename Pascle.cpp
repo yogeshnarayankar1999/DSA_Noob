63b18dd8dd5e7d5784c82e787c1667cd2b97a84b
Yogesh <narayankaryogesh7@gmail.com>
2021-08-09 21:32:34 +0530

    Pascal Code in CPP

---
+++ Pascle.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter The Number: "<<endl;
+    cin>>n;
+    
+    int pascal=1;
+
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0; j<=i; j++)
+        {
+            if(i==0 || j==0)
+            {
+                pascal=1;
+            }
+            else
+            {
+                pascal = pascal *(i-j+1)/j;
+            }
+            cout<<pascal<<" ";
+        }
+        cout<<endl;
+
+    }
+    getch();
+}
